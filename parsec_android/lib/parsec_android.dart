87a78b3314a46f1abec764993089ffcb6f2cab5e
Rodrigo Ferreira <rodrigo_fpinho@hotmail.com>
2023-01-26 12:06:08 -0300

    DF-0734 Creating parsec_android (#3)
    ## Requires https://github.com/oxeanbits/parsec_flutter/pull/2
    
    ## Description
    This PR aims to create the Android implementation of `parsec` plugin.
    
    - creates a symbolic link of the `equations-parser` folder inside the `android` module of this plugin to make it easier to use;
      - `ln -s ../../../../equations_parser ./equations_parser `
    - configure `CMakeLists` and load parsec lib (C++) in `android` module;
    - implement `parsec` method in `kotlin` code of the `android` module;
    - implements the `channel` that invokes the above native android code;

---
+++ parsec_android/lib/parsec_android.dart
@@ -0,0 +1,25 @@
+// You have generated a new plugin project without specifying the `--platforms`
+// flag. A plugin project with no platform support was generated. To add a
+// platform, run `flutter create -t plugin --platforms <platforms> .` under the
+// same directory. You can also find a detailed instruction on how to add
+// platforms in the `pubspec.yaml` at
+// https://flutter.dev/docs/development/packages-and-plugins/developing-packages#plugin-platforms.
+
+import 'package:flutter/services.dart';
+import 'package:parsec_platform_interface/parsec_platform_interface.dart';
+
+const MethodChannel _channel = MethodChannel('parsec_android');
+
+class ParsecAndroid extends ParsecPlatform {
+  static void registerWith() {
+    ParsecPlatform.instance = ParsecAndroid();
+  }
+
+  @override
+  Future<String?> nativeEval(String equation) {
+    return _channel.invokeMethod<String>(
+      'nativeEval',
+      {"equation": equation},
+    ).then((String? result) => result);
+  }
+}
