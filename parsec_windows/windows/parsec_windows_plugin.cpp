6b92d92b6beb56783cc5ce54c26f7b75f1a99f6d
Rodrigo Ferreira <rodrigo_fpinho@hotmail.com>
2023-01-31 14:52:23 -0300

    fix

--- parsec_windows/windows/parsec_windows_plugin.cpp
+++ parsec_windows/windows/parsec_windows_plugin.cpp
@@ -7,18 +7,16 @@
 #include <flutter/plugin_registrar_windows.h>
 #include <flutter/standard_method_codec.h>
 
-#include "mpParser.h"
-#include "mpDefines.h"
-
 #include <memory>
 #include <sstream>
 
 #include <cstring>
 #include <string>
 #include <iostream>
+#include "equationsParser.h"
 
 using namespace std;
-using namespace mup;
+using namespace EquationsParser;
 
 namespace parsec_windows {
 
@@ -43,67 +41,6 @@ void ParsecWindowsPlugin::RegisterWithRegistrar(
 ParsecWindowsPlugin::ParsecWindowsPlugin() {}
 
 ParsecWindowsPlugin::~ParsecWindowsPlugin() {}
-
-void replaceAll(std::string& source, const std::string& from, const std::string& to) {
-    std::string newString;
-    newString.reserve(source.length());  // avoids a few memory allocations
-
-    std::string::size_type lastPos = 0;
-    std::string::size_type findPos;
-
-    while(std::string::npos != (findPos = source.find(from, lastPos)))
-    {
-        newString.append(source, lastPos, findPos - lastPos);
-        newString += to;
-        lastPos = findPos + from.length();
-    }
-
-    // Care for the rest after last occurrence
-    newString += source.substr(lastPos);
-
-    source.swap(newString);
-}
-
-std::string CalcJson(std::string& input) {
-    ParserX parser(pckALL_NON_COMPLEX);
-
-    Value ans;
-    parser.DefineVar(_T("ans"), Variable(&ans));
-
-    stringstream_type ss;
-
-    ss << _T("{");
-
-    try
-    {
-        parser.SetExpr(input);
-        ans = parser.Eval();
-
-        std::string ansString = ans.AsString();
-
-        replaceAll(ansString, "\"", "\\\"");
-
-        ss << _T("\"val\": \"") << ansString << _T("\"");
-        ss << _T(",\"type\": \"") << ans.GetType() << _T("\"");
-    }
-    catch(ParserError &e)
-    {
-        if (e.GetPos() != -1) {
-            string_type error = e.GetMsg();
-            ss << _T("\"error\": \"") << error << _T("\"");
-        }
-    }
-    catch(std::runtime_error &)
-    {
-        string_type error = "Error: Runtime error";
-        ss << _T("\"error\": \"") << error << _T("\"");
-    }
-
-    ss << _T("}");
-
-    return ss.str();
-}
-
 
 void HandleNativeEval(const flutter::MethodCall<flutter::EncodableValue> &method_call, std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
     const flutter::EncodableMap &arguments = std::get<flutter::EncodableMap>(*method_call.arguments());
