7b7c214f73d03c6bb36710fa4bba18ad5755d5d0
Rodrigo Ferreira <rodrigo_fpinho@hotmail.com>
2023-01-31 14:54:43 -0300

    fix

--- parsec_windows/lib/parsec_windows.dart
+++ parsec_windows/lib/parsec_windows.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-import 'package:parsec_platform_interface/parsec_eval_exception.dart';
 import 'package:flutter/services.dart';
 import 'package:parsec_platform_interface/parsec_platform_interface.dart';
 
@@ -12,31 +10,7 @@ class ParsecWindows extends ParsecPlatform {
 
   @override
   Future<dynamic> nativeEval(String equation) {
-    return _channel.invokeMethod(
-      'nativeEval',
-      {'equation': equation},
-    ).then((result) => parseNativeEvalResult(result));
-  }
-
-  dynamic parseNativeEvalResult(String jsonString) {
-    var jsonData = jsonDecode(jsonString);
-    var val = jsonData['val'];
-    var type = jsonData['type'];
-    var error = jsonData['error'];
-
-    if (error != null) {
-      throw ParsecEvalException(error);
-    }
-
-    switch (type) {
-      case 'i':
-        return int.parse(val);
-      case 'f':
-        return double.parse(val);
-      case 'b':
-        return val == 'true';
-      default:
-        return val;
-    }
+    return _channel.invokeMethod('nativeEval', {'equation': equation}).then(
+        (result) => parseNativeEvalResult(result));
   }
 }
