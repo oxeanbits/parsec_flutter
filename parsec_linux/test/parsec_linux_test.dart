802c639b2307c2316c1cace46fa220d4c1933194
Rodrigo Ferreira <rodrigo_fpinho@hotmail.com>
2023-01-31 13:31:09 -0300

    fix

--- parsec_linux/test/parsec_linux_test.dart
+++ parsec_linux/test/parsec_linux_test.dart
@@ -1,29 +1,21 @@
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:parsec_linux/parsec_linux.dart';
-import 'package:parsec_linux/parsec_linux_platform_interface.dart';
-import 'package:parsec_linux/parsec_linux_method_channel.dart';
-import 'package:plugin_platform_interface/plugin_platform_interface.dart';
-
-class MockParsecLinuxPlatform
-    with MockPlatformInterfaceMixin
-    implements ParsecLinuxPlatform {
-
-  @override
-  Future<String?> nativeEval() => Future.value('42');
-}
+import 'package:parsec_platform_interface/parsec_platform_interface.dart';
 
 void main() {
-  final ParsecLinuxPlatform initialPlatform = ParsecLinuxPlatform.instance;
+  TestWidgetsFlutterBinding.ensureInitialized();
 
-  test('$MethodChannelParsecLinux is the default instance', () {
-    expect(initialPlatform, isInstanceOf<MethodChannelParsecLinux>());
+  const MethodChannel channel = MethodChannel('parsec_linux');
+
+  setUp(() {
+    channel.setMockMethodCallHandler((MethodCall methodCall) async {
+      return null;
+    });
   });
 
-  test('nativeEval', () async {
-    ParsecLinux parsecLinuxPlugin = ParsecLinux();
-    MockParsecLinuxPlatform fakePlatform = MockParsecLinuxPlatform();
-    ParsecLinuxPlatform.instance = fakePlatform;
-
-    expect(await parsecLinuxPlugin.nativeEval('4 * 10 + 2'), '42');
+  test('registers instance', () {
+    ParsecLinux.registerWith();
+    expect(ParsecPlatform.instance, isA<ParsecLinux>());
   });
 }
